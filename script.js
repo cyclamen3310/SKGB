54995abe0b93482cb03d8eadcfe0117995a2cbfd
cyclamen3310 <64729640+cyclamen3310@users.noreply.github.com>
2020-06-07 21:50:04 +0900

    相手の映像を合成

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ myVideo.style.display = 'none';
 //自身の映像用キャンバス
 const myCanvas = document.getElementById('myCanvas');
 //描画用コンテキスト
-const context = myCanvas.getContext('2d');
+const myContext = myCanvas.getContext('2d');
 /*
   自身のカメラ同期
 */
@@ -64,7 +64,7 @@ function syncCamera(is_front){
     // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
     localStream = stream;
     //キャンバスに自身の映像を描画
-    drawCanvas();
+    drawMyCanvas();
   }).catch( error => {
     // 失敗時にはエラーログを出力
     console.error('mediaDevice.getUserMedia() error:', error);
@@ -72,27 +72,35 @@ function syncCamera(is_front){
   });
 }
 
+//自身のカメラの映像をキャンバスに描画
+function drawMyCanvas(){
+  //キャンバスにカメラ映像を描画
+  drawCanvas(myVideo, myCanvas, myContext);
+  //クロマキー処理
+  chromaKey();
+  //ループ
+  requestAnimationFrame(drawMyCanvas);
+}
+
 //videoの映像をcanvasに描画する
-function drawCanvas(){
+function drawCanvas(_video, _canvas, _context){
   //キャンバスを画面サイズに合わせる
-  if(myVideo.videoHeight != 0 && myVideo.videoWidth != 0)
+  if(_video.videoHeight != 0 && _video.videoWidth != 0)
   {
-    myCanvas.width = window.parent.screen.width;
-    myCanvas.height = (myVideo.videoHeight / myVideo.videoWidth) * window.parent.screen.width;
+    _canvas.width = window.parent.screen.width;
+    _canvas.height = (_video.videoHeight / _video.videoWidth) * window.parent.screen.width;
   }
   //描画
-  context.drawImage(myVideo, 0, 0, myCanvas.width, myCanvas.height);
-  chromaKey();
-  requestAnimationFrame(drawCanvas);
+  _context.drawImage(_video, 0, 0, _canvas.width, _canvas.height);
 }
 
 // 消す色と閾値
-var chromaKeyColor = {r: 0, g: 255, b: 0},
-colorDistance = 30;
+let chromaKeyColor = {r: 0, g: 255, b: 0};
+let colorDistance = 30;
 
 // クロマキー処理
-var chromaKey = function () {
-var imageData = context.getImageData(0, 0, myCanvas.width, myCanvas.height),
+const chromaKey = function () {
+let imageData = myContext.getImageData(0, 0, myCanvas.width, myCanvas.height),
     data = imageData.data;
 
 // dataはUint8ClampedArray
@@ -102,7 +110,7 @@ var imageData = context.getImageData(0, 0, myCanvas.width, myCanvas.height),
 // n から n+4 までが1つのピクセルの情報となる
 
 for (var i = 0, l = data.length; i < l; i += 4) {
-    var target = {
+    let target = {
             r: data[i],
             g: data[i + 1],
             b: data[i + 2]
@@ -118,12 +126,12 @@ for (var i = 0, l = data.length; i < l; i += 4) {
 
 // 書き換えたdataをimageDataにもどし、描画する
 imageData.data = data;
-context.putImageData(imageData, 0, 0);
+myContext.putImageData(imageData, 0, 0);
 };
 
 //近似色の計算
 // r,g,bというkeyを持ったobjectが第一引数と第二引数に渡される想定
-var getColorDistance = function (rgb1, rgb2) {
+const getColorDistance = function (rgb1, rgb2) {
   // 三次元空間の距離が返る
   return Math.sqrt(
       Math.pow((rgb1.r - rgb2.r), 2) +
@@ -133,7 +141,7 @@ var getColorDistance = function (rgb1, rgb2) {
 };
 
 //色選択コントロール
-var color = document.getElementById('color');
+const color = document.getElementById('color');
 //色変更で透過色変更
 color.addEventListener('change', function () {
     // フォームの値は16進カラーコードなのでrgb値に変換する
@@ -141,7 +149,7 @@ color.addEventListener('change', function () {
 });
 
 //カラーコードをrgbに変換
-var color2rgb = function (color) {
+const color2rgb = function (color) {
   color = color.replace(/^#/, '');
   return {
       r: parseInt(color.substr(0, 2), 16),
@@ -151,12 +159,28 @@ var color2rgb = function (color) {
 };
 
 //閾値入力コントロール
-var distance = document.getElementById('distance');
+const distance = document.getElementById('distance');
 distance.style.textAlign = 'right';
 //閾値変更時
 distance.addEventListener('change', function () {
     colorDistance = this.value;
 });
+
+//相手のカメラ
+const theirVideo = document.getElementById('their-video');
+theirVideo.style.display = 'none';
+//相手の映像用キャンバス
+const theirCanvas = document.getElementById('theirCanvas');
+//描画用コンテキスト
+const theirContext = theirCanvas.getContext('2d');
+
+//相手のカメラの映像をキャンバスに描画
+function drawTheirCanvas(){
+  //キャンバスにカメラ映像を描画
+  drawCanvas(theirVideo, theirCanvas, theirContext);
+  //ループ
+  requestAnimationFrame(drawMyCanvas);
+}
 
 // 発信処理
 document.getElementById('make-call').onclick = () => {
@@ -169,9 +193,8 @@ document.getElementById('make-call').onclick = () => {
 const setEventListener = mediaConnection => {
   mediaConnection.on('stream', stream => {
     // video要素にカメラ映像をセットして再生
-    const videoElm = document.getElementById('their-video')
-    videoElm.srcObject = stream;
-    videoElm.play();
+    theirVideo.srcObject = stream;
+    theirVideo.play();
   });
 }
 
